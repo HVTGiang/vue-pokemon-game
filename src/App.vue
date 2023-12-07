54c76504d3e23f92ffea559d98202c47e2d62755
BlackDang <hoangvutruonggiang28@gmail.com>
2023-12-07 11:41:44 +0700

    build: init pokemon project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="container">Pokemon game</div>
+</template>
+
+<style scoped>
+#container {
+  width: 100vw;
+  height: 100vh;
+  line-height: 100vh;
+  text-align: center;
+  font-size: 50px;
+  color: var(--color-text-main);
+  background-color: var(--color-background-main);
+}
+</style>
